ab8042f64ffec4eb83e5cdf042469049433bb77c
eleonoraa007 <hrubenja.eleonora@gmail.com>
2025-02-11 10:17:52 +0100

    Test login input and intercept request

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -17,3 +17,23 @@ Cypress.Commands.add("register", (username, email, password) => {
   });
   cy.url().should("include", "/dashboard");
 });
+
+Cypress.Commands.add("login", (email, password) => {
+  cy.visit("/login");
+
+  cy.get("#email").should("be.visible").type(email);
+  cy.get("#password").should("be.visible").type(password);
+
+  cy.intercept({
+    method: "POST",
+    url: "**/login*",
+  }).as("loginRequest");
+
+  cy.contains("button", "Sign in").click();
+
+  cy.wait("@loginRequest", { timeout: 10000 }).then((interception) => {
+    expect(interception.response.statusCode).to.be.oneOf([200, 201]);
+  });
+
+  cy.url().should("include", "/dashboard");
+});
